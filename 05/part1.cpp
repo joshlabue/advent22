4182c687911a177812403bcab6b7c3c06bc11877
Joshua LaBue <labuejl@clarkson.edu>
2022-12-05 17:49:24 -0500

    Day 5: Clean up code and add comments

--- 05/part1.cpp
+++ 05/part1.cpp
@@ -3,50 +3,87 @@
 #include <sstream>
 #include <vector>
 
+/*
+Empty vector used to initialize new stacks
+*/
+const std::vector<char> emptyVector;
+
 int main() {
     std::ifstream input("input.txt");
 
     std::vector<std::vector<char>> stacks;
-    std::vector<char> emptyVector;
-    stacks.push_back(emptyVector);
-
 
     while(!input.eof()) {
         std::string line;
         getline(input, line);
 
-        if(line.size() == 0) continue;
-        if(line.at(1) >= '1' && line.at(1) <= '9') {
+        // If the row is empty, skip it
+        if(line.size() == 0) continue; 
+
+        // If the row is just the stack numbers, skip it
+        if(line.at(1) >= '1' && line.at(1) <= '9') { 
             continue;
         }
 
+        // If the row is an instruction
         if(line.at(0) == 'm') {
+            // Utilize a stringstream for the extraction operator
             std::stringstream ss(line);
 
+            /*
+            Params Array
+            [0] Number of crates to move
+            [1] Stack to take crates from
+            [2] Stack to move crates to 
+            */
             int params[3] = {0};
+
+            // Read the instruction into the stringstream and extract the numbers from it
             while(!ss.eof()) {
                 std::string paramStr;
                 ss >> paramStr;
                 int param = atoi(paramStr.c_str());
+
+                //Check if the param is a number (it will be zero if it is just text)s
                 if(param != 0) {
+                    /*
+                    Calculate the index of the next non-zero element in the params array,
+                    and write the param to it.
+                    */
                     params[(params[0] != 0) + (params[1] != 0)] = param;
                 }
             }
-            // printf("move %d crates from stack %d to stack %d\n", params[0], params[1], params[2]);
 
+            /*
+            Decrement the written index of the stacks,
+            since the stacks vector indexes from 0, not 1
+            */
+            params[1]--;
+            params[2]--;
+            
+
+            /*
+            One crate at a time,
+            - Copy the topmost crate to the destination stack
+            - Delete the original crate on the source stack
+            */
             for(int i = 0; i < params[0]; i++) {
                 char crate = stacks[params[1]].back();
                 stacks[params[2]].push_back(crate);
                 stacks[params[1]].pop_back();
             }
         }
+        // If the row is describing the initial stack configuration
         else {
-            int stack = 1;
+            int stack = 0;
             for(int i = 1; i < line.size(); i += 4) {
+
+                // Initialize an empty stack if it doesn't already exist
                 if(stacks.size() <= stack) {
                     stacks.push_back(emptyVector);
                 }
 
+                // If there is a crate present, put it on the stack vector
                 if(line.at(i) != ' ') {
                     std::string character;
                     character = line.at(i);
@@ -57,11 +94,13 @@ int main() {
             }
         }
 
-
         input.peek();
     }
 
-    for(int i = 1; i < stacks.size(); i++) {
+    /*
+    Print the topmost crate on each stack
+    */
+    for(int i = 0; i < stacks.size(); i++) {
         printf("%c", stacks[i].back());
     }
     printf("\n");
--- 05/part2.cpp
+++ 05/part2.cpp
@@ -3,37 +3,70 @@
 #include <sstream>
 #include <vector>
 
+/*
+Empty vector used to initialize new stacks
+*/
+const std::vector<char> emptyVector;
+
 int main() {
     std::ifstream input("input.txt");
 
     std::vector<std::vector<char>> stacks;
-    std::vector<char> emptyVector;
-    stacks.push_back(emptyVector);
 
     while(!input.eof()) {
         std::string line;
         getline(input, line);
 
-        if(line.size() == 0) continue;
-        if(line.at(1) >= '1' && line.at(1) <= '9') {
+        // If the row is empty, skip it
+        if(line.size() == 0) continue; 
+
+        // If the row is just the stack numbers, skip it
+        if(line.at(1) >= '1' && line.at(1) <= '9') { 
             continue;
         }
 
+        // If the row is an instruction
         if(line.at(0) == 'm') {
-
+            // Utilize a stringstream for the extraction operator
             std::stringstream ss(line);
 
+            /*
+            Params Array
+            [0] Number of crates to move
+            [1] Stack to take crates from
+            [2] Stack to move crates to 
+            */
             int params[3] = {0};
+
+            // Read the instruction into the stringstream and extract the numbers from it
             while(!ss.eof()) {
                 std::string paramStr;
                 ss >> paramStr;
                 int param = atoi(paramStr.c_str());
+
+                //Check if the param is a number (it will be zero if it is just text)s
                 if(param != 0) {
+                    /*
+                    Calculate the index of the next non-zero element in the params array,
+                    and write the param to it.
+                    */
                     params[(params[0] != 0) + (params[1] != 0)] = param;
                 }
             }
-            // printf("move %d crates from stack %d to stack %d\n", params[0], params[1], params[2]);
 
+            /*
+            Decrement the written index of the stacks,
+            since the stacks vector indexes from 0, not 1
+            */
+            params[1]--;
+            params[2]--;
+
+            /*
+            Copy the bottom-most crate that must be moved from the source to the destination stack
+            Then remove it from the original stack.
+
+            Repeat until all crates have been moved
+            */
             for(int i = 0; i < params[0]; i++) {
                 int crateToRemove = stacks[params[1]].size() - params[0] + i;  
                 char crate = stacks[params[1]].at(crateToRemove);
@@ -41,16 +74,17 @@ int main() {
                 stacks[params[1]].erase(stacks[params[1]].begin() + crateToRemove);
             }
         }
+        // If the row is describing the initial stack configuration
         else {
-
-            int stack = 1;
+            int stack = 0;
             for(int i = 1; i < line.size(); i += 4) {
 
-                // If the stack list is too small, initialize a new stack
+                // Initialize an empty stack if it doesn't already exist
                 if(stacks.size() <= stack) {
                     stacks.push_back(emptyVector);
                 }
 
+                // If there is a crate present, put it on the stack vector
                 if(line.at(i) != ' ') {
                     std::string character;
                     character = line.at(i);
@@ -65,7 +99,10 @@ int main() {
         input.peek();
     }
 
-    for(int i = 1; i < stacks.size(); i++) {
+    /*
+    Print the topmost crate on each stack
+    */
+    for(int i = 0; i < stacks.size(); i++) {
         printf("%c", stacks[i].back());
     }
     
